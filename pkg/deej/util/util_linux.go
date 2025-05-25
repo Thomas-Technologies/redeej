e88acb104309e5d5f0877f809f02d6c4d6071efd
MichaelThomas0721 <michaelthomas0721@icloud.com>
2025-05-25 00:46:26 -0400

    Implemented window name util for hyprland

--- pkg/deej/util/util_linux.go
+++ pkg/deej/util/util_linux.go
@@ -1,9 +1,40 @@
 package util
 
 import (
-	"errors"
+	"fmt"
+	"os/exec"
+	"strings"
 )
 
+// Wayland doesn't have a universal way of getting window names
+// Logic has to be implemented per desktop environment
+// I use hyprland currently so thats the one I implemented
 func getCurrentWindowProcessNames() ([]string, error) {
-	return nil, errors.New("Not implemented")
+	winName, err := getCurrentWindow_Hyprland()
+	if err != nil {
+		return []string{}, err
+	}
+	return []string{winName}, nil
+}
+
+func getCurrentWindow_Hyprland() (string, error) {
+	cmd := exec.Command("hyprctl", "activewindow")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	windowInfo := strings.TrimSpace(string(output))
+	lines := strings.Split(windowInfo, "\n")
+
+	for _, line := range lines {
+		splitLine := strings.Split(line, ":")
+
+		fmt.Println(splitLine[0])
+		if strings.TrimSpace(splitLine[0]) == "class" {
+			return strings.TrimSpace(splitLine[1]), nil
+		}
+	}
+	fmt.Println("Error: Hyprctl didn't return a window with a class")
+
+	return "", nil
 }
